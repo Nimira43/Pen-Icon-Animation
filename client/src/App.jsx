491b5d24b86122063efebbef45264ac81116d9f8
Nick Rabone <n.rabone72@aol.com>
2024-04-16 01:18:32 +0100

    Setting up routes and nested routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import HomeLayout from './pages/HomeLayout'
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  // Error,
+} from './pages'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomeLayout />
-  },
-  {
-    path: '/about',
-    element: (
-      <div>
-        <h1>home</h1>
-      </div>
-    ),  
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,  
+      },
+      {
+        path: 'login',
+        element: <Login />,  
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,  
+      },
+    ],
   },
 ])
 

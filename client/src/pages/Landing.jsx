103305e49164c39604dbba3751b0e926c87f1ccb
Nick Rabone <n.rabone72@aol.com>
2024-04-16 03:29:49 +0100

    Enhancing routing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,17 +1,33 @@
-import styled from 'styled-components'
-
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`
+// import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main-5.svg'
+import { Link } from 'react-router-dom'
+import { Logo } from '../components'
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <StyledBtn>Styled Button</StyledBtn>
-    </div>
-  )  
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+            eligendi neque quidem. Repellendus neque esse dolorum ad,
+            dignissimos similique facilis quaerat dolor, accusantium eius alias
+            soluta, quae qui tempore porro.
+          </p>
+          <Link to='/register' className='btn register-link'>Register</Link>
+          <Link to='/login' className='btn'>Login / Demo User</Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img'/>
+      </div>
+    </Wrapper>
+  );  
 }
+
 export default Landing

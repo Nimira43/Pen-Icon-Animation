bd0a633023aa2edbf04e6bc15aae7278a4298f96
Nick Rabone <n.rabone72@aol.com>
2024-06-21 10:38:06 +0100

    Continue to modify colours and theme

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 // import styled from 'styled-components'
 import Wrapper from '../assets/wrappers/LandingPage'
-import main from '../assets/images/main-5.svg'
+import main from '../assets/images/main.svg'
 import { Link } from 'react-router-dom'
 import { Logo } from '../components'
 

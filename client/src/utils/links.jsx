b6ba870e2e8f625055c062f15070f2034fd46f72
Nick Rabone <n.rabone72@aol.com>
2024-06-22 22:43:45 +0100

    Correcting error in links.jsx

--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -5,7 +5,14 @@ import { IoBarChartSharp } from 'react-icons/io5'
 import { ImProfile } from 'react-icons/im'
 import { MdAdminPanelSettings } from 'react-icons/md'
 
-
+const links = [
+    {
+        text: 'add job', path: '.', icon: <FaWpforms />
+    },
+    {
+        text: 'all jobs', path: 'all-jobs', icon: <MdQueryStats />
+    },
+    {
         text: 'stats', path: 'stats', icon: <IoBarChartSharp />
     },
     {

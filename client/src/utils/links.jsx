64b22bc8eb60c558c2818c022e83f5c9ff4674c2
Nick Rabone <n.rabone72@aol.com>
2024-06-22 22:39:34 +0100

    Update

--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -5,14 +5,7 @@ import { IoBarChartSharp } from 'react-icons/io5'
 import { ImProfile } from 'react-icons/im'
 import { MdAdminPanelSettings } from 'react-icons/md'
 
-const links = [
-    {
-        text: 'add job', path: '.', icon: <FaWpforms />
-    },
-    {
-        text: 'all jobs', path: 'all-jobs', icon: <MdQueryStats />
-    },
-    {
+
         text: 'stats', path: 'stats', icon: <IoBarChartSharp />
     },
     {

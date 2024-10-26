7f40cd32a3a50c35f75b727e63d2a7c6853d8106
Nimira43 <n.rabone72@aol.com>
2024-10-26 23:18:41 +0100

    Created landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,76 @@
+import main from '../assets/images/main.svg'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
 const Landing = () => {
-  return <h1>Landing</h1>
-}
-export default Landing
+  return (
+    <StyledWrapper>
+      <nav>
+        <h1 className='logo'>WorkSearch</h1>
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h3>Find<span> Your</span> Destiny</h3>
+          <p>Your dream job is just a click away! Discover thousands of opportunities, connect with top employers, and take the next step in your career journey. Empower your future with our intuitive job search platform.</p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.section`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+    margin-bottom: 1.5rem;
+  }
+  p {
+    line-height: 2;
+    color: var(--text-secondary-color);
+    margin-bottom: 1.5rem;
+    max-width: 35em;
+  }
+  .register-link {
+    margin-right: 1rem;
+  }
+  .main-img {
+    display: none;
+  }
+  .btn {
+    padding: 0.75rem 1rem;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 400px;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;

2ef85009e261bf2d384b68ee52ecaa50eec82d11
Nimira43 <n.rabone72@aol.com>
2024-10-26 23:58:17 +0100

    Login form

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,30 @@
-const Logo = () => {
-  return <h3 className='logo'>WorkSearch</h3>;
-};
+import Logo  from '../components/Logo'
+import FormRow from '../components/FormRow'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { Link } from 'react-router-dom'
 
-export default Logo;
+const Login = () => {
+  return (
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type='email' name='email' defaultValue='user@email.com' />
+        <FormRow type='password' name='password' defaultValue='Password' />
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          Explore the App
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  )
+}
+export default Login

7c50800d038885fbe6b884faea8966cc23f90d24
Nimira43 <n.rabone72@aol.com>
2024-10-26 21:24:23 +0100

    Project rebooted

--- client/src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-// import logo from "../assets/images/logo.svg";
-
-const Logo = () => {
-  // return <img src={logo} alt="worksearch" className="logo" />;
-  return <h4 className="logo">WorkSearch</h4> 
-}
-
-export default Logo

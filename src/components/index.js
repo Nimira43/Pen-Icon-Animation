d62a59330ffbc636ba53ebe655c088b3774a15cc
Nimira43 <n.rabone72@aol.com>
2024-12-13 22:27:03 +0000

    Set up Navbar component. Also added imports to the components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { default as Logo } from './Logo'
+import { default as FormRow } from './FormRow'
+import { default as Navbar } from './Navbar'
+import { default as BigSidebar } from './BigSidebar'
+import { default as SmallSidebar } from './SmallSidebar'
+

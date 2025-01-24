6d92001c9357e2b71ef0f8137543746e35061a6a
Nimira43 <n.rabone72@aol.com>
2025-01-24 19:25:11 +0000

    Hittin some nasty errors at the moment. Will troubleshoot

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,28 +1,11 @@
-import {main} from '../assets/images/landing-offer.svg'
-import { Link } from 'react-router-dom'
-import Wrapper from '../assets/wrappers/LandingPage'
-
 const Landing = () => {
   return (
-    <Wrapper>
-      <nav>
-        <h1 className='logo'>Worksearch</h1>
-      </nav>
-      <div className="container page">
-        <div className="info">
-          <h1>Find Your Dream Job</h1>
-          <p>Discover countless opportunities and connect with employers worldwide. Our platform makes job searching simple and effective, empowering you to take the next step in your career journey.</p>
-          <Link to='/register' className='btn register-link'>
-            Register
-          </Link>  
-          <Link to='/login' className='btn'>
-            Login
-          </Link>  
-        </div>
-        <img src={main} alt='Worksearch' className='img main-img' />
-      </div>
-    </Wrapper>
+    <div>
+      <h1>Landing Page</h1>      
+    </div>
   )
 }
 
 export default Landing
+
+

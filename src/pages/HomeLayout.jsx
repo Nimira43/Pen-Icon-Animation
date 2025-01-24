023e5d2454a3ad037f318ff892d8d256c9e0829d
Nimira43 <n.rabone72@aol.com>
2025-01-24 16:35:35 +0000

    Brought in Outlet on HomeLayout page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,8 +1,10 @@
+import { Outlet } from 'react-router-dom'
+
 const HomeLayout = () => {
   return (
-    <div>
-      <h1>HomeLayout Page</h1>      
-    </div>
+    <>
+      <Outlet />      
+    </>
   )
 }
 

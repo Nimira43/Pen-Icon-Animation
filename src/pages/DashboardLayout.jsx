373f5b36b019e98f47fb85bb89df033a43994596
Nimira43 <n.rabone72@aol.com>
2025-04-12 23:17:48 +0100

    Created showSidebar state variable.

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,8 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/Dashboard'
 import { BigSidebar, Navbar, SmallSidebar } from '../components'
+import { useState } from 'react'
 
 const DashboardLayout = () => {
+  const user = { name: 'Lenny' }
+  const [showSidebar, setShowSidebar] = useState(false)
+
+
   return (
     <Wrapper>
       <main className='dashboard'>

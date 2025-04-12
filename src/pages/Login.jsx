074c0ad460588a78f7775a2b48488dedd8e164ee
Nimira43 <n.rabone72@aol.com>
2025-04-12 21:01:53 +0100

    Login page done.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom'
+import  Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import FormRow from '../components/FormRow'
+import Logo from '../components/Logo'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to='/register'>Register Page</Link>      
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
+        <button type='submit' className='btn btn-block'>Submit</button>
+        <p>
+          Not signed up?
+          <Link to='/register' className='link'>Register</Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 

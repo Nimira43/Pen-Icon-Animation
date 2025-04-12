2cec9c386675f9d53eedbfaa7d5c9c6fd3022aa2
Nimira43 <n.rabone72@aol.com>
2025-04-13 00:40:40 +0100

    Starting work on Navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import Wrapper from '../assets/wrappers/Navbar'
-
+import { AiOutlineHome } from 'react-icons/ai'
+import { LiaAlignCenterSolid } from 'react-icons/lia'
+import Logo from './Logo'
 const Navbar = () => {
   return (
     <Wrapper>
-      Navbar
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn'>
+          <LiaAlignCenterSolid className='' />
+        </button>
+        <div>
+          <Logo />
+          <h4>Dashboard</h4>
+        </div>
+        <div className="btn-container">Toggle / Logout</div>
+      </div>
     </Wrapper>
   )
 }

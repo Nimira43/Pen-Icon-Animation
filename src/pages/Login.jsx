a070fb40e09aeb18a638a6de1764e975073c2b87
Nimira43 <n.rabone72@aol.com>
2025-04-12 21:05:58 +0100

    Added Explore button.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,6 +12,7 @@ const Login = () => {
         <FormRow type='email' name='email' />
         <FormRow type='password' name='password' />
         <button type='submit' className='btn btn-block'>Submit</button>
+        <button type='button' className='btn btn-block'>Explore</button>
         <p>
           Not signed up?
           <Link to='/register' className='link'>Register</Link>
@@ -21,43 +22,7 @@ const Login = () => {
   )
 }
 
-
-
-
-
-
 export default Login
 
-// import Logo  from '../components/Logo'
-// import FormRow  from '../components/FormRow'
-// import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
 
-// import { Link } from 'react-router-dom'
-
-// const Login = () => {
-//   return (
-//     <Wrapper>
-//       <form className='form'>
-//         <Logo />
-//         <h4>Login</h4>
-//         <FormRow type='email' name='email' defaultValue='john@gmail.com' />
-//         <FormRow type='password' name='password' defaultValue='secret123' />
-//         <button type='submit' className='btn btn-block'>
-//           submit
-//         </button>
-//         <button type='button' className='btn btn-block'>
-//           explore the app
-//         </button>
-//         <p>
-//           Not a member yet?
-//           <Link to='/register' className='member-btn'>
-//             Register
-//           </Link>
-//         </p>
-//       </form>
-//     </Wrapper>
-//   )
-// }
-
-// export default Login;
 
